1d3b0b020e542c2587d2d483831a63fac840e3ed
Yakovlev Artyom <yakovlev@fruitfulcode.com>
2013-11-15 18:03:33 +0200

    back content.php

--- content.php
+++ content.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * @package Fruitful theme
+ * @package WordPress
+ * @subpackage Fruitful theme
  * @since Fruitful theme 1.0
  */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('blog_post'); ?>>
-	<?php $day 			= get_the_date('d'); 
+	<?php $day 		 = get_the_date('d'); 
 		  $month_abr = get_the_date('M');
 	?>
 	<div class="date_of_post">
@@ -16,15 +17,14 @@
 	<div class="post-content">	
 	<header class="post-header">
 		<h1 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'fruitful' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-		
-		<?php //if ( 'post' == get_post_type() ) : ?>
-		<!--<div class="entry-meta"> -->
-			<?php //fruitful_posted_on(); ?>
-		<!--</div><!-- .entry-meta -->
-		<?php //endif; ?>
+		<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+		<div class="entry-thumbnail">
+			<?php the_post_thumbnail(); ?>
+		</div>
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+	<?php if ( (is_search())) : // Only display Excerpts for Search ?>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
@@ -40,29 +40,7 @@
 			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'fruitful' ), __( '1 Comment', 'fruitful' ), __( '% Comments', 'fruitful' ) ); ?></span>
 		<?php } ?>
 		
-			<span class="author-link"><a href="<?php print esc_url( get_author_posts_url( get_the_author_meta( 'ID' ))); ?>"><?php print get_the_author(); ?></a></span>
-					<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'fruitful' ) );
-				if ( $categories_list && fruitful_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'fruitful' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'fruitful' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tag-links">
-				<?php printf( __( 'Tagged %1$s', 'fruitful' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-		<?php edit_post_link( __( 'Edit', 'fruitful' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php fruitful_entry_meta(); ?>
 	</footer><!-- .entry-meta -->
 	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
