442cf7bc1adb46e4f2a41c7961e1acfffff5b48a
Artyom Yakovlev <ManiakMill@gmail.com>
2013-12-19 19:47:08 +0200

    bug.fix. small-menu-select

--- js/small-menu-select.js
+++ js/small-menu-select.js
@@ -1,61 +1,67 @@
-(function($){
-$.fn.mobileMenu = function(options) {
+(function ($) {
+    $.fn.mobileMenu = function (options) {
 
-	var defaults = {
-		defaultText: 'Navigate to...',
-		className: 'select-menu',
-		subMenuClass: 'sub-menu',
-		subMenuDash: '&ndash;'
-	},
-	settings = $.extend( defaults, options ),
-	el = $(this);
+        var defaults = {
+            defaultText: 'Navigate to...',
+            className: 'select-menu',
+            subMenuClass: 'sub-menu',
+            subMenuDash: '&ndash;'
+        },
+            settings = $.extend(defaults, options),
+            el = $(this);
+        i = 0;
+        this.each(function () {
+            // ad class to submenu list
+            if ($(this).parents('aside').length > 0) return;
+            i++;
+            el.find('ul').addClass(settings.subMenuClass);
+            $(this).attr('id', 'main-menu-' + i);
+            // Create base menu
+            $('<select />', {
+                'id': 'mobile-menu-' + i,
+                'class': settings.className
+            }).insertAfter($('#main-menu-' + i));
 
-	this.each(function(){
-	// ad class to submenu list
-	el.find('ul').addClass(settings.subMenuClass);
+            // Create default option
+            $('<option />', {
+                "value": '#',
+                "text": settings.defaultText
+            }).appendTo('#' + 'mobile-menu-' + i);
 
-	// Create base menu
-		$('<select />',{
-								'class' : settings.className
-							}).insertAfter( el );
+            // Create select option from menu
+            $('#main-menu-' + i).find('a').each(function () {
+                var $this = '',
+                    optText = '',
+                    optSub = '',
+                    len = '',
+                    dash = '';
 
-	// Create default option
-		$('<option />', {
-								"value"	: '#',
-								"text"	: settings.defaultText
-							}).appendTo( '.' + settings.className );
+                $this = $(this), optText = '&nbsp;' + $this.text(), optSub = $this.parents('.' + settings.subMenuClass), len = optSub.length;
 
-	// Create select option from menu
-		el.find('a').each(function(){
-		var $this = $(this),
-		optText	= '&nbsp;' + $this.text(),
-		optSub	= $this.parents( '.' + settings.subMenuClass ),
-		len	= optSub.length,
-		dash;
 
-		// if menu has sub menu
-		if( $this.parents('ul').hasClass( settings.subMenuClass ) ) {
-			dash = Array( len+1 ).join( settings.subMenuDash );
-			optText = dash + optText;
-		}
+                // if menu has sub menu
+                if ($this.parents('ul').hasClass(settings.subMenuClass)) {
+                    dash = Array(len + 1).join(settings.subMenuDash);
+                    optText = dash + optText;
+                }
 
-	// Now build menu and append it
-	$('<option />', {
-							"value"	: this.href,
-							"html"	: optText,
-							"selected" : (this.href == window.location.href)
-						}).appendTo( '.' + settings.className );
+                // Now build menu and append it
+                $('<option />', {
+                    "value": this.href,
+                    "html": optText,
+                    "selected": (this.href == window.location.href)
+                }).appendTo('#' + 'mobile-menu-' + i);
 
-	}); // End el.find('a').each
+            }); // End el.find('a').each
+            // Change event on select element
+            $('.' + settings.className).change(function () {
+                var locations = $(this).val();
+                if (locations !== '#') {
+                    window.location.href = $(this).val();
+                };
+            });
 
-// Change event on select element
-$('.' + settings.className).change(function(){ var locations = $(this).val(); 
-		if( locations !== '#' ) { 
-				window.location.href = $(this).val();
-		};
-});
-
-}); 
-return this;
-};
+        });
+        return this;
+    };
 })(jQuery);
