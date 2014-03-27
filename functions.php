34ff115d0e37271de019dd5107330af5817d4b08
Nickolay Buinenko <buinenko@fruitfulcode.com>
2014-03-27 17:42:39 +0200

    Remove old, (not used) functions

--- functions.php
+++ functions.php
@@ -1032,32 +1032,6 @@ if (class_exists('Woocommerce')) {
 
 
 /*rewrite pagenavi for woocommerce*/
-function pagenavi( $p = 2 ) { // pages will be show before and after current page
-	echo '<div class="nav-links shop aligncenter">';
-		  if ( is_singular() ) return; // don't show in single page
-		  global $wp_query, $paged;
-		  $max_page = $wp_query->max_num_pages;
-		  if ( $max_page == 1 ) return; // don't show when only one page
-		  if ( empty( $paged ) ) $paged = 1;
-		  // echo '<span class="pages">Page: ' . $paged . ' of ' . $max_page . ' </span> '; // pages
-		  if ( $paged > $p + 1 ) p_link( 1, 'First' );
-		  if ( $paged > $p + 2 ) echo '... ';
-				echo '<div class="nav-previous ">'; next_posts_link( 'previous' ); echo '</div>';
-				echo '<div class="pages-links">';
-					for( $i = $paged - $p; $i <= $paged + $p; $i++ ) { // Middle pages
-						if ( $i > 0 && $i <= $max_page ) $i == $paged ? print "<span class='page-numbers current'>{$i}</span> " : p_link( $i );
-					 }
-				echo '</div>';
-		  if ( $paged < $max_page - $p - 1 ) echo '... ';
-		  if ( $paged < $max_page - $p ) p_link( $max_page, 'Last' );
-				echo '<div class="nav-next">'; previous_posts_link( 'next' ); echo '</div>';
-	echo '</div>';
-}
-function p_link( $i, $title = '' ) {
-  if ( $title == '' ) $title = "Page {$i}";
-  echo "<a class='page-numbers' href='", esc_html( get_pagenum_link( $i ) ), "' title='{$title}'>{$i}</a> ";
-}
-
 function fruitful_wp_corenavi() {  
 	global $wp_query, 
 			$wp_rewrite;  
