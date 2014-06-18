91cc7be00d1c5facc3e67bf57c9c03e723d0d615
Nickolay Buinenko <buinenko@fruitfulcode.com>
2014-06-18 12:51:57 +0300

    Fix default widgets for homepage sidebar

--- sidebar-homepage.php
+++ sidebar-homepage.php
@@ -9,7 +9,7 @@
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php do_action( 'before_sidebar' ); ?>
-			<?php if ( ! dynamic_sidebar( 'sidebar-5' ) ) : ?>
+			<?php if ( ! dynamic_sidebar( 'sidebar-4' ) ) : ?>
 				
 				<aside id="search" class="widget widget_search">
 					<?php get_search_form(); ?>
