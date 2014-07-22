9012c452673ce489e4ee64d6c703cb853f7cff98
Nickolay Buinenko <buinenko@fruitfulcode.com>
2014-07-22 15:22:56 +0300

    Remove header from homepage

--- content-page.php
+++ content-page.php
@@ -12,10 +12,11 @@
 	<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
 		<div class="entry-thumbnail"><?php the_post_thumbnail(); ?></div>
 	<?php endif; ?>
-		
+	<?php if (!is_front_page()) {?>	
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
+	<?php } ?>
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'fruitful' ), 'after' => '</div>' ) ); ?>
