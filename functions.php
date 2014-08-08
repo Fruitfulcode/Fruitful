807034b7c7b04441b93aa75bd86e17c236ad46d1
Nickolay Buinenko <buinenko@fruitfulcode.com>
2014-08-08 13:22:49 +0300

    Fix slider styles, message if slides is empty

--- functions.php
+++ functions.php
@@ -392,65 +392,73 @@ function fruitful_get_slider() {
 	
 	/*Full Backend Options*/
 	if(isset($theme_options['slides']) && (count($theme_options['slides']) > 0)) {
-		 if ($theme_options['select_slider'] == "1") {
-				if ($slider_options == 1) {
-					$slider_ .= '<div class="main-slider-container">';
-				} else {
-					$slider_ .= '<div class="main-slider-container fullwidth">';
-				}
-					$slider_ .= '<section class="slider">';
-						$slider_ .= '<div class= "flexslider" id="' . $id . '">';
-							$slider_ .= '<ul class="slides">';
-							foreach ($theme_options['slides'] as $key=>$slide) {
-								$path_to_img = $val = '';
-								$val = wp_get_attachment_image_src( esc_attr($slide['attach_id']), 'full');
-								$path_to_img = esc_url_raw($val[0]);
-								$slider_ .= '<li>';
-									if (!empty($slide['link'])) {
-										if (!empty($slide['is_blank'])) {
-											$slider_ .= '<a href="'.esc_url($slide['link']).'" target="_blank">';
+		foreach ($theme_options['slides'] as $key=>$slide) {
+			$path_to_img = $val = $empty_slider_html = '';
+			$val = wp_get_attachment_image_src( esc_attr($slide['attach_id']), 'full');
+			$path_to_img = esc_url_raw($val[0]);
+		}
+		
+		if ($path_to_img){
+			if ($theme_options['select_slider'] == "1") {
+					if ($slider_options == 1) {
+						$slider_ .= '<div class="main-slider-container">';
+					} else {
+						$slider_ .= '<div class="main-slider-container fullwidth">';
+					}
+						$slider_ .= '<section class="slider">';
+							$slider_ .= '<div class= "flexslider" id="' . $id . '">';
+								$slider_ .= '<ul class="slides">';
+								foreach ($theme_options['slides'] as $key=>$slide) {
+									$slider_ .= '<li>';
+										if (!empty($slide['link'])) {
+											if (!empty($slide['is_blank'])) {
+												$slider_ .= '<a href="'.esc_url($slide['link']).'" target="_blank">';
+											} else {
+												$slider_ .= '<a href="'.esc_url($slide['link']).'">';
+											}	
+												$slider_ .= '<img src="'.$path_to_img.'" />';
+											$slider_ .= '</a>';	
 										} else {
-											$slider_ .= '<a href="'.esc_url($slide['link']).'">';
-										}	
 											$slider_ .= '<img src="'.$path_to_img.'" />';
-										$slider_ .= '</a>';	
-									} else {
-										$slider_ .= '<img src="'.$path_to_img.'" />';
-									}
-								$slider_ .= '</li>';
-							}
-							$slider_ .= '</ul></div></section></div>';
-		} else if ($theme_options['select_slider'] == "2") {
-				
-				if ($slider_options == 1) {
-					$slider_ .= '<div class="slider-wrapper '. $theme_options['nv_skins'] .'">';
-				} else {
-					$slider_ .= '<div class="slider-wrapper fullwidth '. $theme_options['nv_skins'] .'">'; 
-				}
-				
-					$slider_ .= '<div id="nivo-slider-'. $id . '" class="nivoSlider">';
-					foreach ($theme_options['slides'] as $key=>$slide) {
-						$path_to_img = $val = '';
-						$val = wp_get_attachment_image_src( esc_attr($slide['attach_id']), 'full');
-						$path_to_img = esc_url_raw($val[0]);
-						
-						if (!empty($slide['link'])) {
-							if (!empty($slide['is_blank'])) {
-								$slider_ .= '<a href="'.esc_url($slide['link']).'" target="_blank">';
+										}
+									$slider_ .= '</li>';
+								}
+								$slider_ .= '</ul></div></section></div>';
+								
+			} else if ($theme_options['select_slider'] == "2") {
+					
+					if ($slider_options == 1) {
+						$slider_ .= '<div class="main-slider-container slider-wrapper '. $theme_options['nv_skins'] .'">';
+					} else {
+						$slider_ .= '<div class="main-slider-container slider-wrapper fullwidth '. $theme_options['nv_skins'] .'">'; 
+					}
+					
+						$slider_ .= '<div id="nivo-slider-'. $id . '" class="nivoSlider">';
+						foreach ($theme_options['slides'] as $key=>$slide) {
+							if (!empty($slide['link'])) {
+								if (!empty($slide['is_blank'])) {
+									$slider_ .= '<a href="'.esc_url($slide['link']).'" target="_blank">';
+								} else {
+									$slider_ .= '<a href="'.esc_url($slide['link']).'">';
+								}	
+									$slider_ .= '<img src="'. $path_to_img .'" data-thumb="'. $path_to_img .'" alt="" />';
+								$slider_ .= '</a>';	
 							} else {
-								$slider_ .= '<a href="'.esc_url($slide['link']).'">';
-							}	
 								$slider_ .= '<img src="'. $path_to_img .'" data-thumb="'. $path_to_img .'" alt="" />';
-							$slider_ .= '</a>';	
-						} else {
-							$slider_ .= '<img src="'. $path_to_img .'" data-thumb="'. $path_to_img .'" alt="" />';
-						}
-					}	
+							}
+						}	
+						$slider_ .= '</div>';
 					$slider_ .= '</div>';
-				$slider_ .= '</div>';
-		}	
+			}	
+		} else {
+			$slider_ = '<div class="main-slider-container">';
+				$slider_ .= '<section class="slider"><h3 class="no-slider-text">'. __('Please add images for slider in theme options!', 'fruitful') .'</h3></section>';
+			$slider_ .= '</div>';
+		}
 	} else {
-		$slider_ .= '<h3 class="no-slider-text">'. __('Please add images for slider in theme options!', 'fruitful') .'</h3>';
+		$slider_ = '<div class="main-slider-container">';
+			$slider_ .= '<section class="slider"><h3 class="no-slider-text">'. __('Please add images for slider in theme options!', 'fruitful') .'</h3></section>';
+		$slider_ .= '</div>';
 	}
 	
 	if (!empty($slider_options)) {
@@ -546,21 +554,22 @@ function fruitful_get_socials_icon () {
 	$out = '';
 	$theme_options  = fruitful_ret_options("fruitful_theme_options"); 
 	
-	if(!empty($theme_options['facebook_url'])) 		{ $out .= '<a class="facebook" 	href="'	. esc_url($theme_options['facebook_url']) 	. '" target="_blank"><i class="fa"></i></a>';	}
-	if(!empty($theme_options['twitter_url']))		{ $out .= '<a class="twitter" 	href="'	. esc_url($theme_options['twitter_url']) 	. '" target="_blank"><i class="fa"></i></a>'; }
-	if(!empty($theme_options['linkedin_url'])) 		{ $out .= '<a class="linkedin" 	href="'	. esc_url($theme_options['linkedin_url']) 	. '" target="_blank"><i class="fa"></i></a>'; }
-	if(!empty($theme_options['myspace_url'])) 		{ $out .= '<a class="myspace" 	href="'	. esc_url($theme_options['myspace_url']) 	. '" target="_blank"></a>'; }	
-	if(!empty($theme_options['googleplus_url'])) 	{ $out .= '<a class="googleplus" href="'. esc_url($theme_options['googleplus_url']) . '" target="_blank"><i class="fa"></i></a>'; }		
-	if(!empty($theme_options['dribbble_url'])) 	 	{ $out .= '<a class="dribbble" 	href="'			. esc_url($theme_options['dribbble_url']) 	. '" target="_blank"><i class="fa"></i></a>'; }		
-	if(!empty($theme_options['skype_link'])) 		{ $out .= '<a class="skype" 	href="skype:' 	. esc_attr($theme_options['skype_link'])	. '?call"><i class="fa"></i></a>'; }		
-	if(!empty($theme_options['flickr_link'])) 		{ $out .= '<a class="flickr" 	href="'		. esc_url($theme_options['flickr_link']) 	. '" target="_blank"><i class="fa"></i></a>'; }		
-	if(!empty($theme_options['youtube_url'])) 		{ $out .= '<a class="youtube" 	href="'		. esc_url($theme_options['youtube_url']) 	. '" target="_blank"><i class="fa"></i></a>'; }		
-	if(!empty($theme_options['rss_link'])) 			{ $out .= '<a class="rss" 		href="'		. esc_url($theme_options['rss_link']) 		. '" target="_blank"><i class="fa"></i></a>'; }			
-	if(!empty($theme_options['vk_link'])) 			{ $out .= '<a class="vk" 		href="'		. esc_url($theme_options['vk_link']) 		. '" target="_blank"><i class="fa"></i></a>'; }			
-	if(!empty($theme_options['instagram_url']))		{ $out .= '<a class="instagram"	href="'		. esc_url($theme_options['instagram_url'])	. '" target="_blank"><i class="fa"></i></a>'; }			
-	if(!empty($theme_options['pinterest_url']))		{ $out .= '<a class="pinterest"	href="'		. esc_url($theme_options['pinterest_url'])	. '" target="_blank"><i class="fa"></i></a>'; }			
-	if(!empty($theme_options['yelp_url']))			{ $out .= '<a class="yelp"		href="'		. esc_url($theme_options['yelp_url'])		. '" target="_blank"></a>'; }			
-	if(!empty($theme_options['email_link'])) 		{ $out .= '<a class="email" 	href="mailto:'		. sanitize_email($theme_options['email_link']) . '"><i class="fa"></i></a>'; }			
+	if(!empty($theme_options['facebook_url'])) 		{ $out .= '<a class="facebook" 	href="'	. 		esc_url($theme_options['facebook_url']) 	. '" target="_blank"><i class="fa"></i></a>'; }
+	if(!empty($theme_options['twitter_url']))		{ $out .= '<a class="twitter" 	href="'	. 		esc_url($theme_options['twitter_url']) 		. '" target="_blank"><i class="fa"></i></a>'; }
+	if(!empty($theme_options['linkedin_url'])) 		{ $out .= '<a class="linkedin" 	href="'	. 		esc_url($theme_options['linkedin_url']) 	. '" target="_blank"><i class="fa"></i></a>'; }
+	if(!empty($theme_options['myspace_url'])) 		{ $out .= '<a class="myspace" 	href="'	. 		esc_url($theme_options['myspace_url']) 		. '" target="_blank"></a>'; }	
+	if(!empty($theme_options['googleplus_url'])) 	{ $out .= '<a class="googleplus" href="'. 		esc_url($theme_options['googleplus_url']) 	. '" target="_blank"><i class="fa"></i></a>'; }		
+	if(!empty($theme_options['dribbble_url'])) 	 	{ $out .= '<a class="dribbble" 	href="'	.		esc_url($theme_options['dribbble_url']) 	. '" target="_blank"><i class="fa"></i></a>'; }		
+	if(!empty($theme_options['skype_link'])) 		{ $out .= '<a class="skype" 	href="skype:'.	esc_attr($theme_options['skype_link'])		. '?call"><i class="fa"></i></a>'; }		
+	if(!empty($theme_options['flickr_link'])) 		{ $out .= '<a class="flickr" 	href="' . 		esc_url($theme_options['flickr_link']) 		. '" target="_blank"><i class="fa"></i></a>'; }		
+	if(!empty($theme_options['youtube_url'])) 		{ $out .= '<a class="youtube" 	href="'	. 		esc_url($theme_options['youtube_url']) 		. '" target="_blank"><i class="fa"></i></a>'; }		
+	if(!empty($theme_options['rss_link'])) 			{ $out .= '<a class="rss" 		href="'	.		esc_url($theme_options['rss_link']) 		. '" target="_blank"><i class="fa"></i></a>'; }			
+	if(!empty($theme_options['vk_link'])) 			{ $out .= '<a class="vk" 		href="'	.		esc_url($theme_options['vk_link'])			. '" target="_blank"><i class="fa"></i></a>'; }			
+	if(!empty($theme_options['instagram_url']))		{ $out .= '<a class="instagram"	href="'	.		esc_url($theme_options['instagram_url'])	. '" target="_blank"><i class="fa"></i></a>'; }			
+	if(!empty($theme_options['pinterest_url']))		{ $out .= '<a class="pinterest"	href="'	.		esc_url($theme_options['pinterest_url'])	. '" target="_blank"><i class="fa"></i></a>'; }			
+	if(!empty($theme_options['yelp_url']))			{ $out .= '<a class="yelp"		href="'	.		esc_url($theme_options['yelp_url'])			. '" target="_blank"></a>'; }			
+	if(!empty($theme_options['email_link'])) 		{ $out .= '<a class="email" 	href="mailto:'. sanitize_email($theme_options['email_link']). '"><i class="fa"></i></a>'; }			
+	if(!empty($theme_options['github_link'])) 		{ $out .= '<a class="github" 	href="'	.		esc_url($theme_options['github_link']) 		. '" target="_blank"><i class="fa"></i></a>'; }			
 	
 	echo '<div class="social-icon">' . $out . '</div>';
 }
