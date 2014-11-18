088843fed722f13926c2f1dd5918caf49bd43999
jpbandalaria <jp_kybordista@yahoo.com>
2014-11-18 08:48:07 +0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -45,20 +45,6 @@ jQuery(document).ready(function($) {
 		$('.menu li:has(ul)').mobileMenuDropdown();
 		$(window).resize();
 		
-		if ($('.resp_full_width_menu').length > 0){
-			var respMenuMaxHeight = $(window).height()/2 + 50;
-			$('.resp_full_width_menu .site-header .menu_wrapper').css({'max-height' : respMenuMaxHeight});
-			$('.resp_full_width_menu button.navbar-toggle').live('click', function(){
-				if ($(this).hasClass('collapsed')){
-					$('.resp_full_width_menu .menu_wrapper').fadeIn('slow').removeClass('collapse');
-					$(this).removeClass('collapsed');
-				} else {
-					$('.resp_full_width_menu .menu_wrapper').fadeOut('slow').addClass('collapse');
-					$(this).addClass('collapsed');
-				}
-			});
-		}
-		
 		$('.resp_full_width_menu .language_switcher .current').live('click', function(e) {
 			var el = $(this);
 			var child = el.find('#lang-select-popup');
