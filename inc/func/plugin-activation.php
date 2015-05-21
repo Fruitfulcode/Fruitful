b8d029efdd137966637a39f4ce552d680a5bf084
Yakovlev Artyom <yakovlev@fruitfulcode.com>
2015-05-21 17:38:00 +0300

    remove from tgm plugin screen_icon func.

--- inc/func/plugin-activation.php
+++ inc/func/plugin-activation.php
@@ -376,10 +376,6 @@ if ( ! class_exists( 'TGM_Plugin_Activation' ) ) {
 
             ?>
             <div class="fruitful wrap">
-
-                <?php if ( version_compare( $this->wp_version, '3.8', '<' ) ) {
-                    screen_icon( apply_filters( 'fruitful_default_screen_icon', 'themes' ) );
-                } ?>
                 <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
                 <?php $plugin_table->prepare_items(); ?>
 
@@ -1587,9 +1583,6 @@ if ( ! class_exists( 'TGMPA_List_Table' ) ) {
 
                 // Wrap the install process with the appropriate HTML.
                 echo '<div class="fruitful wrap">';
-                    if ( version_compare( TGM_Plugin_Activation::$instance->wp_version, '3.8', '<' ) ) {
-                        screen_icon( apply_filters( 'fruitful_default_screen_icon', 'themes' ) );
-                    }
                     echo '<h2>' . esc_html( get_admin_page_title() ) . '</h2>';
                     // Process the bulk installation submissions.
                     $installer->bulk_install( $sources );
