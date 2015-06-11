52629ce0ed1ab0a7aa6f465f1f928c8af17614bb
Eugene <kirpichev@fruitfulcode.com>
2015-06-11 16:55:19 +0300

    Woocommerce
    "Proceed to checkout" & "Continue shopping" buttons color- inline style

--- functions.php
+++ functions.php
@@ -1029,8 +1029,8 @@ if ( ! function_exists( 'fruitful_get_responsive_style' ) ) {
 					
 					$style_		 .= 'button, input[type="button"], input[type="submit"], input[type="reset"]{background-color : '.$btn_color.' !important; } ';
 					$style_		 .= 'body a.btn.btn-primary, body button.btn.btn-primary, body input[type="button"].btn.btn-primary , body input[type="submit"].btn.btn-primary {background-color : '.$btn_color.' !important; }';
-					$woo_style_  .= '.woocommerce table.my_account_orders .order-actions .button, .woocommerce-page table.my_account_orders .order-actions .button{background-color : '.$btn_color.' !important; }';
-					$style_ 	 .= '.nav-links.shop .pages-links .page-numbers, .nav-links.shop .nav-next a, .nav-links.shop .nav-previous a{background-color : '.$btn_color.' !important; }';
+					$woo_style_  .= '.woocommerce table.my_account_orders .order-actions .button, .woocommerce-page table.my_account_orders .order-actions .button, .woocommerce-page .wc-proceed-to-checkout a, .woocommerce-page .button.wc-backward {background-color : '.$btn_color.' !important; }';
+					$style_ 	 .= '.nav-links.shop .pages-links .page-numbers, .nav-links.shop .nav-next a, .nav-links.shop .nav-previous a {background-color : '.$btn_color.' !important; }';
 				}
 				
 				if (!empty($theme_options['btn_active_color'])) {
@@ -1041,7 +1041,7 @@ if ( ! function_exists( 'fruitful_get_responsive_style' ) ) {
 					$style_ .= 'input[type="submit"]:hover, input[type="submit"]:active, input[type="submit"]:focus{background-color : '.$btn_active_color.' !important; }';
 					$style_ .= 'input[type="reset"]:hover, input[type="reset"]:active, input[type="reset"]:focus{background-color : '.$btn_active_color.' !important; }';
 					$style_	.= 'body a.btn.btn-primary:hover, body button.btn.btn-primary:hover, body input[type="button"].btn.btn-primary:hover , body input[type="submit"].btn.btn-primary:hover {background-color : '.$btn_active_color.' !important; }';
-					$woo_style_  .= '.woocommerce table.my_account_orders .order-actions .button:hover, .woocommerce-page table.my_account_orders .order-actions .button:hover{background-color : '.$btn_active_color.' !important; }';
+					$woo_style_  .= '.woocommerce table.my_account_orders .order-actions .button:hover, .woocommerce-page table.my_account_orders .order-actions .button:hover, .woocommerce-page .wc-proceed-to-checkout a:hover, .woocommerce-page .button.wc-backward:hover {background-color : '.$btn_active_color.' !important; }';
 					$style_ .= '.nav-links.shop .pages-links .page-numbers:hover, .nav-links.shop .nav-next a:hover, .nav-links.shop .nav-previous a:hover, .nav-links.shop .pages-links .page-numbers.current{background-color : '.$btn_active_color.' !important; }';
 				}
 				
