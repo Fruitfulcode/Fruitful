62316301c9bd927678bce42620bbb879529eae5d
Artyom Yakolvev <yakovlev@fruitfulcode.com>
2017-03-09 18:02:11 +0200

    improve: Outdated copies of WooCommerce template file

--- woocommerce/global/wrapper-start.php
+++ woocommerce/global/wrapper-start.php
@@ -2,7 +2,16 @@
 /**
  * Content wrappers
  *
- * @author 		Fruitful
+ * This template can be overridden by copying it to yourtheme/woocommerce/global/wrapper-start.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
  * @package 	WooCommerce/Templates
  * @version     1.6.4
  */
@@ -14,17 +23,24 @@ $woo_sidebar = fruitful_get_woo_sidebar();
 
 switch( $template ) {
 	case 'twentyeleven' :
-		echo '<div id="primary"><div id="content" role="main">';
-		break;
-	case 'twentytwelve' :
-		echo '<div id="primary" class="site-content"><div id="content" role="main">';
-		break;
-	case 'twentythirteen' :
-		echo '<div id="primary" class="site-content"><div id="content" role="main" class="entry-content twentythirteen">';
-		break;
-	case 'twentyfourteen' :
-		echo '<div id="primary" class="content-area"><div id="content" role="main" class="site-content twentyfourteen"><div class="tfwc">';
-		break;
+    case 'twentyeleven' :
+        echo '<div id="primary"><div id="content" role="main" class="twentyeleven">';
+        break;
+    case 'twentytwelve' :
+        echo '<div id="primary" class="site-content"><div id="content" role="main" class="twentytwelve">';
+        break;
+    case 'twentythirteen' :
+        echo '<div id="primary" class="site-content"><div id="content" role="main" class="entry-content twentythirteen">';
+        break;
+    case 'twentyfourteen' :
+        echo '<div id="primary" class="content-area"><div id="content" role="main" class="site-content twentyfourteen"><div class="tfwc">';
+        break;
+    case 'twentyfifteen' :
+        echo '<div id="primary" role="main" class="content-area twentyfifteen"><div id="main" class="site-main t15wc">';
+        break;
+    case 'twentysixteen' :
+        echo '<div id="primary" class="content-area twentysixteen"><main id="main" class="site-main" role="main">';
+        break;
 	case 'fruitful' :
 	    $theme_options = fruitful_get_theme_options();
 		$prod_num_row_class = '';
@@ -44,7 +60,7 @@ switch( $template ) {
 		}		
 
 		break;
-	default :
-		echo '<div id="container"><div id="content" role="main">';
-		break;
+    default :
+        echo '<div id="container"><div id="content" role="main">';
+        break;
 }
--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -2,16 +2,25 @@
 /**
  * Single Product Image
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/product-image.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.6.4
+ * @version     2.6.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-global $post, $woocommerce, $product;
+global $post, $product;
 
 ?>
 <div class="images">
