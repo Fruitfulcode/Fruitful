e83040bc57b6645a9ee3242f6ca7553db45cb457
zelenetska <iryna.zelenetska@gmail.com>
2017-07-12 18:07:23 +0300

    fix sprintf() bug on instal plugins

--- inc/func/plugin-activation.php
+++ inc/func/plugin-activation.php
@@ -3359,13 +3359,13 @@ if ( ! function_exists( 'tgmpa_load_bulk_installer' ) ) {
 							if ( $this->tgmpa->is_automatic ) {
 								// Automatic activation strings.
 								$this->upgrader->strings['skin_upgrade_start']        = __( 'The installation and activation process is starting. This process may take a while on some hosts, so please be patient.', 'fruitful' );
-								$this->upgrader->strings['skin_update_successful']    = __( '%1$s installed and activated successfully.', 'fruitful' ) . ' <a href="#" class="hide-if-no-js" onclick="%2$s"><span>' . esc_html__( 'Show Details', 'fruitful' ) . '</span><span class="hidden">' . esc_html__( 'Hide Details', 'fruitful' ) . '</span>.</a>';
-								$this->upgrader->strings['skin_upgrade_end']          = __( 'All installations and activations have been completed.', 'fruitful' );
+                                $this->upgrader->strings['skin_update_successful'] = __( '%1$s installed and activated successfully.', 'fruitful' );
+                                $this->upgrader->strings['skin_upgrade_end']          = __( 'All installations and activations have been completed.', 'fruitful' );
 								$this->upgrader->strings['skin_before_update_header'] = __( 'Installing and Activating Plugin %1$s (%2$d/%3$d)', 'fruitful' );
 							} else {
 								// Default installation strings.
 								$this->upgrader->strings['skin_upgrade_start']        = __( 'The installation process is starting. This process may take a while on some hosts, so please be patient.', 'fruitful' );
-								$this->upgrader->strings['skin_update_successful']    = esc_html__( '%1$s installed successfully.', 'fruitful' ) . ' <a href="#" class="hide-if-no-js" onclick="%2$s"><span>' . esc_html__( 'Show Details', 'fruitful' ) . '</span><span class="hidden">' . esc_html__( 'Hide Details', 'fruitful' ) . '</span>.</a>';
+                                $this->upgrader->strings['skin_update_successful'] = esc_html__( '%1$s installed successfully.', 'fruitful' );
 								$this->upgrader->strings['skin_upgrade_end']          = __( 'All installations have been completed.', 'fruitful' );
 								$this->upgrader->strings['skin_before_update_header'] = __( 'Installing Plugin %1$s (%2$d/%3$d)', 'fruitful' );
 							}
