9a680bb84212774bcf57b7386854fe7e7a22f5ca
macmitt2300@gmail.com <macmitt2300@gmail.com>
2017-10-27 10:44:15 -0400

    Added keyboard_image_navigation.js to fix issue #31

---
+++ js/keyboard-image-navigation.js
@@ -0,0 +1,18 @@
+(function ($) {
+    $(document).on('keydown.frutiful', function(e) {
+        var url = false;
+
+        // Left arrow key code.
+        if (e.which === 37) {
+            url = $('.previous-image a').attr('href');
+
+        // Right arrow key code.
+        } else if (e.which === 39) {
+            url = $('.entry-attachment a').attr('href');
+        }
+
+        if (url && (! $('textarea, input').is(':focus'))) {
+            window.location = url;
+        }
+    });
+})(jQuery);
