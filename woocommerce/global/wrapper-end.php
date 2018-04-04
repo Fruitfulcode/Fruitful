3a32be7e57235766bc3b2ac01974e6545a841a71
AZobenko <zobenko@fruitfulcode.com>
2018-04-04 16:54:21 +0300

    Fix Widget layout bug

--- woocommerce/global/wrapper-end.php
+++ woocommerce/global/wrapper-end.php
@@ -11,61 +11,31 @@
  * the readme will list any important changes.
  *
  * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
+ * @author 		WooThemes/Fruitfulcode
  * @package 	WooCommerce/Templates
  * @version     3.3.0
  */
 
-
-$template    = strtolower(get_option( 'template' ));
 $woo_sidebar = fruitful_get_woo_sidebar();
 
-switch( $template ) {
-    case 'twentyeleven' :
-        echo '</div>';
-        get_sidebar( 'shop' );
-        echo '</div>';
-        break;
-    case 'twentytwelve' :
-        echo '</div></div>';
-        break;
-    case 'twentythirteen' :
-        echo '</div></div>';
-        break;
-    case 'twentyfourteen' :
-        echo '</div></div></div>';
-        get_sidebar( 'content' );
-        break;
-    case 'twentyfifteen' :
-        echo '</div></div>';
-        break;
-    case 'twentysixteen' :
-        echo '</main></div>';
-        break;
-	case 'fruitful' :
-			if ($woo_sidebar == 1 || $woo_sidebar == 4 || $woo_sidebar == 5) {
-				echo '</div></div></div>';
-			} elseif($woo_sidebar == 2) {
-				echo '</div></div></div>';
-				echo '<div class="five columns alpha woo-loop-sidebar">';
-					if (is_shop() || is_product_category()) {
-						get_sidebar( 'shop' );
-					} else {
-						get_sidebar( 'product' );
-					}
-				echo '</div>';
-			} else {
-				echo '</div></div></div>';
-				echo '<div class="five columns omega woo-loop-sidebar">';
-					if (is_shop() || is_product_category()){
-						get_sidebar( 'shop' );
-					} else {
-						get_sidebar( 'product' );
-					}
-				echo '</div>';
-			}
-		break;
-	default :
-		echo '</div></div>';
-		break;
+if ($woo_sidebar == 1 || $woo_sidebar == 4 || $woo_sidebar == 5) {
+    echo '</div></div></div>';
+} elseif($woo_sidebar == 2) {
+    echo '</div></div></div>';
+    echo '<div class="five columns alpha woo-loop-sidebar">';
+        if (is_shop() || is_product_category()) {
+            get_sidebar( 'shop' );
+        } else {
+            get_sidebar( 'product' );
+        }
+    echo '</div>';
+} else {
+    echo '</div></div></div>';
+    echo '<div class="five columns omega woo-loop-sidebar">';
+        if (is_shop() || is_product_category()){
+            get_sidebar( 'shop' );
+        } else {
+            get_sidebar( 'product' );
+        }
+    echo '</div>';
 }
--- woocommerce/global/wrapper-start.php
+++ woocommerce/global/wrapper-start.php
@@ -11,56 +11,28 @@
  * the readme will list any important changes.
  *
  * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
+ * @author 		WooThemes/Fruitfulcode
  * @package 	WooCommerce/Templates
  * @version     3.3.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-$template = strtolower(get_option( 'template' ));
 $woo_sidebar = fruitful_get_woo_sidebar();
+$theme_options = fruitful_get_theme_options();
 
-switch( $template ) {
-	case 'twentyeleven' :
-    case 'twentyeleven' :
-        echo '<div id="primary"><div id="content" role="main" class="twentyeleven">';
-        break;
-    case 'twentytwelve' :
-        echo '<div id="primary" class="site-content"><div id="content" role="main" class="twentytwelve">';
-        break;
-    case 'twentythirteen' :
-        echo '<div id="primary" class="site-content"><div id="content" role="main" class="entry-content twentythirteen">';
-        break;
-    case 'twentyfourteen' :
-        echo '<div id="primary" class="content-area"><div id="content" role="main" class="site-content twentyfourteen"><div class="tfwc">';
-        break;
-    case 'twentyfifteen' :
-        echo '<div id="primary" role="main" class="content-area twentyfifteen"><div id="main" class="site-main t15wc">';
-        break;
-    case 'twentysixteen' :
-        echo '<div id="primary" class="content-area twentysixteen"><main id="main" class="site-main" role="main">';
-        break;
-	case 'fruitful' :
-	    $theme_options = fruitful_get_theme_options();
-		$prod_num_row_class = '';
-		if (is_shop() || is_product_category() || is_tax('product_tag')) {
-			if (!empty($theme_options['shop_num_row'])){
-				$prod_num_row = $theme_options['shop_num_row'];
-				$prod_num_row_class = 'prod_num_row-'.$prod_num_row;
-			}
-		}		
+$prod_num_row_class = '';
+if (is_shop() || is_product_category() || is_tax('product_tag')) {
+    if (!empty($theme_options['shop_num_row'])){
+        $prod_num_row = $theme_options['shop_num_row'];
+        $prod_num_row_class = 'prod_num_row-'.$prod_num_row;
+    }
+}
 
-		if ($woo_sidebar == 1 || $woo_sidebar == 4 || $woo_sidebar == 5){
-			echo '<div class="woo-loop-content alpha omega '.$prod_num_row_class.'"><div id="container"><div id="content" role="main">';
-		} elseif($woo_sidebar == 2) {
-			echo '<div class="eleven columns woo-loop-content omega '.$prod_num_row_class.'"><div id="container"><div id="content" role="main">';
-		} else {
-			echo '<div class="eleven columns woo-loop-content alpha '.$prod_num_row_class.'"><div id="container"><div id="content" role="main">';
-		}		
-
-		break;
-    default :
-        echo '<div id="container"><div id="content" role="main">';
-        break;
+if ($woo_sidebar == 1 || $woo_sidebar == 4 || $woo_sidebar == 5){
+    echo '<div class="woo-loop-content alpha omega '.$prod_num_row_class.'"><div id="container"><div id="content" role="main">';
+} elseif($woo_sidebar == 2) {
+    echo '<div class="eleven columns woo-loop-content omega '.$prod_num_row_class.'"><div id="container"><div id="content" role="main">';
+} else {
+    echo '<div class="eleven columns woo-loop-content alpha '.$prod_num_row_class.'"><div id="container"><div id="content" role="main">';
 }
