11ac56d8a27f2ce86f062b52ea3ceefdf7ef657c
Stas Barannik <stas.barannik@matesmarketing.com>
2018-10-18 13:43:11 +0300

    add advertising response

--- vendor/fruitful-app/fruitful-adv/fruitful-advertising.php
+++ vendor/fruitful-app/fruitful-adv/fruitful-advertising.php
@@ -20,7 +20,9 @@
 if ( !class_exists('FruitfulAdv')) {
 	
 	abstract class FruitfulAdv {
-		
+
+		public $product_type;
+
 		/**
 		 * Constructor
 		 **/
@@ -44,18 +46,53 @@ if ( !class_exists('FruitfulAdv')) {
 		}
 
 		/**
+		 * Get product info
+		 */
+		public function get_product_info_array() {
+
+			$info    = array();
+
+			/** @var WP_Theme $theme_info */
+			$theme_info = wp_get_theme();
+
+			if ( $this->product_type === 'theme' ) {
+				$info = array(
+					'product_name' => !empty($theme_info->parent_theme) ? $theme_info->parent_theme:$theme_info->get( 'Name' ),
+					'domain'       => esc_url(site_url()),
+				);
+
+			} else { // this block for plugins only
+				if( !function_exists('get_plugin_data') ){  //Need when theme options update by ajax
+					require_once ABSPATH . 'wp-admin/includes/plugin.php';
+				}
+				$plugin_data = get_plugin_data( $this->root_file );
+				$info  = array(
+					'product_name' => $plugin_data['Name'],
+					'domain'       => esc_url(site_url()),
+				);
+			}
+
+			return $info;
+		}
+
+
+		/**
 		 * Get advertising data from server
 		 **/
 		public function get_advertising() {
 
-			$response = array();
+			$host = 'https://dev.app.fruitfulcode.com/';
+			$uri  = 'api/product/advertising';
 
-			$response['html'] = '<div class="ff-adv-inner"><a href="https://fruitfulcode.com/products/lovely-one-page-wordpress-theme/" class="thumb" target="_blank"><img src="https://38k3yyk4vw814f94ak1lhssj-wpengine.netdna-ssl.com/wp-content/uploads/2014/08/lovely-showcase-420x420.jpg" alt="Lovely | One page WordPress Theme" class="" data-width="420" data-height="420" data-src="https://38k3yyk4vw814f94ak1lhssj-wpengine.netdna-ssl.com/wp-content/uploads/2014/08/lovely-showcase-420x420.jpg"></a><div class="product-footer"><div class="product-desc-container"><a href="https://fruitfulcode.com/products/lovely-one-page-wordpress-theme/" target="_blank">Lovely | One page WordPress Theme</a><p>Template can be used by creative and design agencies, freelance designers, photographers and other creative souls! The greatest feature is ultra easy editing of a modern “skewed” background blocks. Also sections or elements can be animated with prepared animation.</p></div></div></div>';
-			$response['css'] = '.ff-adv-inner {max-width: 290px;} .product-desc-container a {color: #ff5c35;font-weight: bold;text-decoration: none; font-size: 15px;margin-top: 10px;display: inline-block;text-align: center; transition: 0.2s;} .product-desc-container a:focus{outline:none; box-shadow: none;}.product-desc-container a:hover {color: #3e3e3e; transition: 0.3s;}  .thumb{display: block; overflow: hidden;} .thumb img {display:block; width:100%; height:auto; transition: 0.35s;} .thumb:hover img {transform: scale(1.2) rotate(10deg);}';
-			$response['js'] = 'console.log("test");';
+			$params = $this->get_product_info_array();
 
-			return $response;
+			$response = wp_remote_post( $host . $uri, array(
+				'timeout'     => 20,
+				'sslverify' => true,
+				'body'    => $params
+			) );
 
+			return ( !is_wp_error( $response ) ) ? (array) json_decode($response['body']) : false;
 		}
 
 		/**
@@ -64,7 +101,7 @@ if ( !class_exists('FruitfulAdv')) {
 		public function init_advertising_option() {
 
 			if ( ! empty( get_option( 'ffc_advertising_option' ) ) && (bool) get_transient('ffc_advertising_option_actual') === true ) {
-				return;
+				//return;
 			}
 
 			$this->update_advertising();
